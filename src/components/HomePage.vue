495387986b0902f10a5b10a044b9eabfecaf7115
marc-antoine <marc-antoine.da.silva@efrei.net>
2023-10-20 14:28:25 +0200

    AsyncButton

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -6,16 +6,21 @@
       <base-button @click="exploreWebsite" margin="10px" color="warn">Warning Button</base-button>
       <base-button @click="exploreWebsite" margin="10px" color="danger">Danger Button</base-button>
       <base-button :disabled="true" margin="10px" color="primary">Disabled Primary Button</base-button>
+      <async-button @click="exploreWebsite" margin="10px" color="danger">Danger Button</async-button>
+    <base-button :disabled="true" margin="10px" color="primary">Disabled Primary Button</base-button>
     </div>
   </template>
   
   <script>
   import BaseButton from './BaseButton.vue';
+  import AsyncButton from './AsyncButton.vue';
+
   
   export default {
     name: 'HomePage',
     components: {
       'base-button': BaseButton,
+      'async-button': AsyncButton,
     },
     methods: {
       exploreWebsite() {
